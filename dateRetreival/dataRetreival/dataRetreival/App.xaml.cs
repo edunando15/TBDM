36aa83ac5d73207480335041168f1b2deed02731
atomicSteiner <damiano.buzzo@studenti.unicam.it>
2025-03-02 12:59:38 +0100

    [add]example of data retriever in c#

---
+++ dateRetreival/dataRetreival/dataRetreival/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace dataRetreival
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
---
+++ dateRetreival/dataRetreival/dataRetreival/Data.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace dataRetreival
+{
+    public class Data
+    {
+        [JsonProperty("id")]
+        public string Id { get; set; }
+
+        [JsonProperty("id_macchina")]
+        public string MachineId { get; set; }
+
+        [JsonProperty("valore")]
+        public double Value { get; set; }
+
+        [JsonProperty("data_registrazione")]
+        public DateTime RegistrationDate { get; set; }
+
+        [JsonProperty("createdAt")]
+        public DateTime CreatedAt { get; set; }
+
+        [JsonProperty("updatedAt")]
+        public DateTime UpdatedAt { get; set; }
+
+        [JsonProperty("nome_parametro")]
+        public string ParameterName { get; set; }
+
+        [JsonProperty("tipo_dato")]
+        public string DataType { get; set; }
+    }
+
+}
